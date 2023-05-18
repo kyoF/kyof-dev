9232687f7f5d227680956cd128c662ea27cb7239
kyoF <fujiki.kyosuke@gmail.com>
2023-05-18 17:14:29 +0900

    tutorial: 12 interface

--- Go/main.go
+++ Go/main.go
@@ -4,159 +4,62 @@ import (
 	"fmt"
 )
 
-// type User struct {
+// type Stringfy interface {
+// 	ToString() string
+// }
+
+// type Person struct {
 // 	Name string
 // 	Age int
-// 	// X, Y int
+// }
+// func (p *Person) ToString() string {
+// 	return fmt.Sprintf("Name=%v, Age=%v", p.Name, p.Age)
 // }
 
-// type Users []*User
-
-// type Users struct {
-// 	Users []*Users
+// type Car struct {
+// 	Number string
+// 	Model string
+// }
+// func (p *Car) ToString() string {
+// 	return fmt.Sprintf("Number=%v, Model=%v", p.Number, p.Model)
 // }
 
-// type T struct {
-// 	User
-// }
-
-// func (u *User) SetName() {
-// 	u.Name = "A"
-// }
-
-// func UpdateUser(user User) {
-// 	user.Name = "A"
-// 	user.Age = 1000
-// }
-
-// func UpdateUser2(user *User) {
-// 	user.Name = "A"
-// 	user.Age = 1000
-// }
-
-// func (u User) SayName() {
-// 	fmt.Println(u.Name)
-// }
-
-// func (u User) SetNameTmp(name string) {
-// 	u.Name = name
-// }
-
-// func (u *User) SetName(name string) {
-// 	u.Name = name
-// }
-
-// func NewUser(name string, age int) *User {
-// 	return &User{Name: name, Age: age}
-// }
-
-type MyInt int
-
-func (mi MyInt) Print() {
-	fmt.Println(mi)
+type MyError struct {
+	Msg string
+	ErrCode int
 }
 
+func (e *MyError) Error() string {
+	return e.Msg
+}
+
+// type Point struct {
+// 	A int
+// 	B string
+// }
+
+// func (p *Point) String() string {
+// 	return fmt.Sprintf("<<%v, %v>>", p.A, p.B)
+// }
+
 func main() {
-	var mi MyInt
-	fmt.Println(mi)
-	fmt.Printf("%T\n", mi)
+	// p := Point{100, "ABC"}
+	// fmt.Println(p)
+	// fmt.Println(p.String())
+	err := MyError{Msg: "error death", ErrCode: 1243}
+	fmt.Println(err.Error())
+	fmt.Println(err.ErrCode)
 
-	mi.Print()
-
-	// i := 100 as MyInt
-	// fmt.Println(mi + i)
-	// m := map[int]User{
-	// 	1: {Name: "user1", Age: 10},
-	// 	2: {Name: "user2", Age: 20},
+	// fmt.Println(err.Msg)
+	// if ok {
+	// 	fmt.Println(e.ErrCode)
 	// }
-	// fmt.Println(m)
-
-	// m2 := map[User]string{
-	// 	{Name: "user1", Age: 10}: "tokyo",
-	// 	{Name: "user2", Age: 20}: "LA",
-	// }
-	// fmt.Println(m2)
-
-	// m3 := make(map[int]User)
-	// fmt.Println(m3)
-	// m3[1] = User{Name: "user3"}
-	// fmt.Println(m3)
-
-	// for _, v := range m {
-	// 	fmt.Println(v)
-	// }
-	// user1 := User{Name: "user1", Age: 10}
-	// user2 := User{Name: "user2", Age: 20}
-	// user3 := User{Name: "user3", Age: 30}
-	// user4 := User{Name: "user4", Age: 40}
-	// users := Users{}
-	// users = append(users, &user1)
-	// users = append(users, &user2)
-	// users = append(users, &user3)
-	// users = append(users, &user4)
-
-	// fmt.Println(users)
-	// for _, v := range users {
-	// 	fmt.Println(*v)
+	// vs := []Stringfy{
+	// 	&Person{Name: "fujiki", Age: 21},
+	// 	&Car{Number: "1234", Model: "Ver3"},
 	// }
 
-	// users2 := make([]*User, 0)
-	// users2 = append(users2, &user1, &user2)
-	// for _, v := range users2 {
-	// 	fmt.Println(*v)
+	// for _, v := range vs {
+	// 	fmt.Println(v.ToString())
 	// }
-	// user := NewUser("fujiki", 10)
-	// fmt.Println(*user)
-	// t := T{User: User{Name: "fujiki", Age: 10}}
-	// fmt.Println(t)
-	// fmt.Println(t.User)
-	// fmt.Println(t.User.Name)
-	// fmt.Println(t.Name)
-
-	// t.User.SetName()
-	// fmt.Println(t)
-	// user1 := User{Name: "fujiki"}
-	// user1.SayName()
-
-	// user1.SetName("A")
-	// user1.SayName()
-
-	// user2 := &User{Name: "kyosuke"}
-	// user2.SayName()
-	// user2.SetNameTmp("B")
-	// user2.SayName()
-
-	// var user1 User
-	// fmt.Println(user1)
-	// user1.Name = "user1"
-	// user1.Age = 10
-	// fmt.Println(user1)
-
-	// user2 := User{}
-	// fmt.Println(user2)
-	// user2.Name = "user2"
-	// fmt.Println(user2)
-
-	// user3 := User{Name: "user3", Age: 30}
-	// fmt.Println(user3)
-
-	// user4 := User{"user4", 40}
-	// fmt.Println(user4)
-
-	// // user5 := User{30, "user5"}
-	// // fmt.Println(user5)
-
-	// user6 := User{Name: "user6"}
-	// fmt.Println(user6)
-
-	// user7 := new(User)
-	// fmt.Println(user7)
-
-	// user8 := &User{}
-	// fmt.Println(user8)
-
-	// UpdateUser(user1)
-	// UpdateUser2(user8)
-	// fmt.Println(user1)
-	// fmt.Println(user8)
 }
