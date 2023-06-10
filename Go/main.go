9cc8ec24536e603944632eda6168138b0bbbd6f1
kyoF <fujiki.kyosuke@gmail.com>
2023-06-10 21:06:54 +0900

    tutorial: 16 標準パッケージ_strvonv

--- Go/main.go
+++ Go/main.go
@@ -1,50 +1,19 @@
 package main
 
 import (
-	// "fmt"
-    "os"
-	"log"
+	"fmt"
+	"strconv"
 )
 
 func main() {
-	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lshortfile)
-	logger.Println("message")
-	log.Println("message")	
+	bt := true
+	fmt.Printf("%T\n", strconv.FormatBool(bt))
 
-	_, err := os.Open("afsdasdf")
-	if err != nil {
-		// log.Fatalln("Exit", err)
-		logger.Fatalln("Exit", err)
-	}
+	i := strconv.FormatInt(-100, 10)
+	fmt.Printf("%v, %T\n", i, i)
+	i2 := strconv.Itoa(100)
+	fmt.Printf("%v, %T\n", i2, i2)
 
-	// log.SetOutput(os.Stdout)
-
-	// log.SetFlags(log.LstdFlags)
-	// log.Println("A")
-	// log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
-	// log.Println("B")
-	// log.SetFlags(log.Ltime | log.Lshortfile)
-	// log.Println("C")
-	// log.SetFlags(log.LstdFlags)
-	// log.SetPrefix("[LOG]")
-	// log.Println("E")
-	
-	// log.Print("log\n")
-	// log.Println("Log2")
-	// log.Printf("log%d\n", 3)
-
-	// log.Fatal("log\n")
-	// log.Fatalln("log2")
-	// log.Fatalf("log%d\n", 3)
-
-	// log.Panic("log\n")
-	// log.Panicln("log2\n")
-	// log.Panicf("log%d\n", 3)
-
-	// f, err := os.Create("test.log")
-	// if err != nil {
-	// 	return
-	// }
-	// log.SetOutput(f)
-	// log.Println("ファイルに書き込む")
+	i10, _ := strconv.Atoi("123")
+	fmt.Println(i10)
 }
