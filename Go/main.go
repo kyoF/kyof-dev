26bd74a52ac4fd84a2ac605f8b8345ac1e21a6ae
kyoF <fujiki.kyosuke@gmail.com>
2023-06-10 22:07:52 +0900

    tutorial: 16 標準パッケージ_regexp

--- Go/main.go
+++ Go/main.go
@@ -2,16 +2,21 @@ package main
 
 import (
 	"fmt"
-    "os"
-    "io/ioutil"
-    "log"
+    "regexp"
 )
 
 func main() {
-    f, _ := os.Open("foo.txt")
-    bs, _ := ioutil.ReadAll(f)
-    fmt.Println(string(bs))
-    if err := ioutil.WriteFile("bar.txt", bs, 0666); err != nil {
-        log.Fatalln(err)
-    }
+    match, _ := regexp.MatchString("A", "ABC")
+    fmt.Println(match)
+
+    a, _ := regexp.Compile("A")
+    match = a.MatchString("ABC")
+    fmt.Println(match)
+
+    b := regexp.MustCompile("A")
+    match = b.MatchString("ABC")
+    fmt.Println(match)
+
+    regexp.MustCompile("\\d")
+    regexp.MustCompile(`\d`)
 }
