dc0e8142c516137d55317aa28c843fa1bc5257f8
kyoF <fujiki.kyosuke@gmail.com>
2023-06-11 14:54:48 +0900

    tutorial: 17 サードパーティーパッケージ_uuid

--- Go/go.mod
+++ Go/go.mod
@@ -2,4 +2,7 @@ module my-local-pkg
 
 go 1.20
 
-require gopkg.in/go-ini/ini.v1 v1.67.0 // indirect
+require (
+	github.com/google/uuid v1.3.0 // indirect
+	gopkg.in/go-ini/ini.v1 v1.67.0 // indirect
+)
--- Go/main.go
+++ Go/main.go
@@ -3,28 +3,12 @@ package main
 import (
     "fmt"
 
-    "gopkg.in/go-ini/ini.v1"
+    "github.com/google/uuid"
 )
 
-type ConfigList struct {
-    Port int
-    DbName string
-    SQLDriver string
-}
-
-var Config ConfigList
-
-func init() {
-    cfg, _ := ini.Load("config.ini")
-    Config = ConfigList{
-        Port: cfg.Section("web").Key("port").MustInt(8080),
-        DbName: cfg.Section("db").Key("name").MustString("example.com"),
-        SQLDriver: cfg.Section("db").Key("driver").String(),
-    }
-}
-
 func main() {
-    fmt.Printf("Port = %v\n", Config.Port)
-    fmt.Printf("DbName = %v\n", Config.DbName)
-    fmt.Printf("SQLDriver = %v\n", Config.SQLDriver)
+    uuidObj, _ := uuid.NewUUID()
+    fmt.Println(uuidObj.String())
+    uuidObj2, _ := uuid.NewRandom()
+    fmt.Println(uuidObj2.String())
 }
