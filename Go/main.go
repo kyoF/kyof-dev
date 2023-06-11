89fe36f62dba58c86b4db5dd3ce41a018393fa7e
kyoF <fujiki.kyosuke@gmail.com>
2023-06-11 13:27:23 +0900

    tutorial: 16 標準パッケージ_json

--- Go/main.go
+++ Go/main.go
@@ -1,15 +1,61 @@
 package main
 
 import (
-	"fmt"
-    "io"
-    "crypto/md5"
+	"encoding/json"
+    "fmt"
+    "log"
+    "time"
 )
 
+type A struct {}
+
+type User struct {
+    ID int `json:"id,omitempty"`
+    Name string `json:"name,omitempty"`
+    Email string `json:"email"`
+    Created time.Time `json:"created"`
+    A *A `json:"A"`
+}
+
+func (u User) MarshalJSON() ([]byte, error) {
+    v, err := json.Marshal(&struct {
+        Name string
+    }{
+        Name: "Mr. " + u.Name,
+    })
+    return v, err
+}
+
+func (u *User) UnmarshalJSON(b []byte) error {
+    type User2 struct {
+        Name string
+    }
+    var u2 User2
+    err := json.Unmarshal(b, &u2)
+    if err != nil {
+        fmt.Println(err)
+    }
+    u.Name = u2.Name+"!"
+    return err
+}
+
 func main() {
-    h := md5.New()
-    io.WriteString(h, "ABVDE")
-    fmt.Println(h.Sum(nil))
-    s := fmt.Sprintf("%x", h.Sum(nil))
-    fmt.Println(s)
+    u := new(User)
+    u.ID = 1
+    u.Name = "fujiki"
+    u.Email = "fujiki@fujiki.com"
+    u.Created = time.Now()
+
+    bs, err := json.Marshal(u)
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Println(string(bs))
+
+    fmt.Println("%T\n", bs)
+    u2 := new(User)
+    if err := json.Unmarshal(bs, &u2); err != nil {
+        fmt.Println(err)
+    }
+    fmt.Println(u2)
 }
