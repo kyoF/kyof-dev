bd32cd0d46defacbe2c846e176035ab563a9cbe9
kyoF <fujiki.kyosuke@gmail.com>
2023-06-11 14:21:56 +0900

    tutorial: 16 標準パッケージ_net/http client

--- Go/main.go
+++ Go/main.go
@@ -2,23 +2,33 @@ package main
 
 import (
     "fmt"
+    "io/ioutil"
+    "net/http"
+    "log"
     "net/url"
 )
 
 func main() {
-    u, _ := url.Parse("https://example.com/search?a=1&b=2#top")
-    fmt.Println(u.Scheme)
-    fmt.Println(u.Host)
-    fmt.Println(u.Path)
-    fmt.Println(u.RawQuery)
-    fmt.Println(u.Fragment)
-    fmt.Println(u.Query())
+    // res, _ := http.Get("https://example.com")
+    // fmt.Println(res.StatusCode)
+    // fmt.Println(res.Proto)
+    // fmt.Println(res.Header["Date"])
+    // fmt.Println(res.Header["Content-Type"])
+    // fmt.Println(res.Request.Method)
+    // fmt.Println(res.Request.URL)
+    // defer res.Body.Close()
+    // body, _ := ioutil.ReadAll(res.Body)
+    // fmt.Println(string(body))
 
-    url := &url.URL{}
-    url.Scheme = "https:"
-    url.Host = "google.com"
-    q := url.Query()
-    q.Set("q", "Golang")
-    url.RawQuery = q.Encode()
-    fmt.Println(url)
+    vs := url.Values{}
+    vs.Add("id", "1")
+    vs.Add("message", "msg")
+    fmt.Println(vs.Encode())
+    res, err := http.PostForm("https://example.com/", vs)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer res.Body.Close()
+    body, _ := ioutil.ReadAll(res.Body)
+    fmt.Println(string(body))
 }
